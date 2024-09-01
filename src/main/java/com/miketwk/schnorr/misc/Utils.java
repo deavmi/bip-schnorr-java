1c28c964b33cc54a78d74f4a423cb6d0be4f2e81
Tristan Brice Velloza Kildaire <deavmi@redxen.eu>
2024-09-01 12:11:35 +0200

    Utils
    - Documenrted

--- src/main/java/com/miketwk/schnorr/misc/Utils.java
+++ src/main/java/com/miketwk/schnorr/misc/Utils.java
@@ -2,8 +2,20 @@ package com.miketwk.schnorr.misc;
 
 import java.util.List;
 
+/**
+ * Utilities
+ * 
+ * @author Tristan Brice Velloza Kildaire (deavmi)
+ */
 public class Utils
 {
+	/**
+	 * Converts a {@link List} of bytes into
+	 * a byte array
+	 * 
+	 * @param bytes the input list
+	 * @return a byte array
+	 */
 	public static byte[] fromListToBytes(final List<Byte> bytes)
 	{
 		byte[] b = new byte[bytes.size()];
